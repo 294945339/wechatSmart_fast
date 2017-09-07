a06a000eb6cc6b29de8f9b49888ce8a0b1d84529
dj <294945339@qq.com>
2017-09-07 12:00:41 +0800

    Initial commit

---
+++ apps/users/adminx.py
@@ -0,0 +1,5 @@
+import xadmin
+
+
+class UserInfoAdmin(object):
+    pass
---
+++ apps/users/views.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+
+# Create your views here.
+
+# 全局 404 处理函数
+def page_not_found(request):
+    from django.shortcuts import render_to_response
+    response = render_to_response('404.html', {})
+    response.status_code = 404
+    return response
+
+
+# # 全局 500 处理函数
+def page_error(request):
+    from django.shortcuts import render_to_response
+    response = render_to_response('500.html', {})
+    response.status_code = 500
+    return response
